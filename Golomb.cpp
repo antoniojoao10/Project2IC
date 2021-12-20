b208dfccff3ebb2d89d38af7ddd12bb81229da75
Pedro Coutinho <pmacoutinho@ua.pt>
2021-12-20 15:36:34 +0000

    better formating

--- Golomb.cpp
+++ Golomb.cpp
@@ -27,29 +27,31 @@ class Golomb{
       }
 
       string encoder(int m, int s){
-        if(s>=0) s = 2 * abs(s);
-        else if(s<0) s = 2 * abs(s) - 1;
-        int k =  ceil(log2(m));
-        int r = s%m;
-        int q = floor(s/m);
+        if (s>=0) 
+          s = 2 * abs(s);
+        else if (s < 0) 
+          s = 2 * abs(s) - 1;
+        
+        int k = ceil(log2(m));
+        int r = s % m;
+        int q = floor(s / m);
+        
         string qo = una[q];
-        int t = pow(2,k) - m;
+        int t = pow(2, k) - m;
         string ro = "";
 
-        if( r < t){
+        if (r < t) {
           string s = integral_to_binary_string(abs(r));
           
-          for( int i = k-1; i>0 ; i--){
+          for (int i=k-1; i>0 ; i--) 
               ro += s[sizeof(s)-i] ;
-          }   
-        }else{
-          r = r + t;
+        } else {
+          r += t;
           
           string s = integral_to_binary_string(abs(r));
           
-          for( int i = k; i>0 ; i--){
-              ro += s[sizeof(s)-i] ;
-          }   
+          for (int i=k; i>0; i--)
+              ro += s[sizeof(s)-i];
         }
 
         string res = qo + ro;
@@ -58,37 +60,61 @@ class Golomb{
 
       int decoder(int m, string sin){
         int k =  ceil(log2(m));
-        int t = pow(2,k) - m;
-        int cnt =sin.length();
-        int sz= sin.length();
-        int s=0;
-        for( int i = sz -1 ; i >=0 ; i--){
-          cnt --;
-          if (sin[sz -1 -i] == '0' && i == sz -1 ) break;
-          if( sin[sz -1 -i] == '1' && i == sz -1 ) s+=1;
-          else if(sin[sz -1 -i]=='1' && sin[sz -1 -i -1] =='1') s+=1;
-          else if(sin[sz -1 -i] =='0' && s!=0) break;
-          else if(sin[sz -1 -i] =='1' && sin[sz -1 -i -1] == '0') s+=1;
+        int t = pow(2, k) - m;
+        int cnt = sin.length();
+        int sz = sin.length();
+        int s = 0;
+        
+        for(int i=sz-1; i>=0; i--) {
+          cnt--;
+          
+          if (sin[sz -1 -i] == '0' && i == sz -1) 
+            break;
+          
+          if (sin[sz -1 -i] == '1' && i == sz -1) 
+            s+=1;
+          else if(sin[sz -1 -i]=='1' && sin[sz -1 -i -1] =='1') 
+            s+=1;
+          else if(sin[sz -1 -i] =='0' && s!=0) 
+            break;
+          else if(sin[sz -1 -i] =='1' && sin[sz -1 -i -1] == '0') 
+            s+=1;
         }
-        int scnt = k -1;
+
+        int scnt = k - 1;
         string x1 = "";
-        for( int i = cnt -1; i>=0 ; i--){
-          cnt=i;
-          if(scnt ==0) break;
+
+        for (int i=cnt-1; i>=0; i--){
+          cnt = i;
+          
+          if(scnt == 0) 
+            break;
+          
           x1 += sin[sz-1-i];
           scnt--;
         }
+
         int x = 0;
-        for( int i = x1.length()-1 ; i >=0 ; i--) if(x1[i] == '1') x += pow(2,x1.length()-1-i);
-        if(x<t){
+
+        for (int i = x1.length()-1 ; i>=0 ; i--) 
+          if(x1[i] == '1') x += pow(2,x1.length()-1-i);
+        
+        if (x < t)
             s = s * m + x;
-        }else{
+        else {
             x = x * 2;
-            if(sin[sz -1 - cnt] == '1') x+=1;
+            
+            if (sin[sz -1 - cnt] == '1') 
+              x+=1;
+            
             s = s * m + x - t;
         }
-        if(s%2!=0) s = -(s/2) - 1;
-        else if(s%2==0) s = s/2 ;
+
+        if ( s%2 != 0) 
+          s = -(s/2) - 1;
+        else if(s%2==0) 
+          s = s / 2;
+        
         return s;
       }
         
@@ -97,7 +123,9 @@ class Golomb{
 int main(int argc, char** argv) {
   Golomb gl;
   string res = gl.encoder(atoi(argv[1]), atoi(argv[2]));
+  
   cout << res << endl;
   cout << gl.decoder(atoi(argv[1]), res) << endl;
+  
   return 0;
 }
